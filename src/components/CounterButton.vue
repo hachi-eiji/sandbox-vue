6cd24597524fe7af967056accdcf5ee2fbf3baeb
Eiji Hachiya <hachiyae@gmail.com>
2024-02-18 11:30:42 +0900

    add: button

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { ref } from 'vue'
+
+const localCounter = ref<number>(0)
+
+const counterStore = useCounterStore()
+
+function clickHandler() {
+  counterStore.increment()
+  localCounter.value++
+}
+</script>
+
+<template>
+  <div>Global {{ counterStore.count }}</div>
+  <div>Local {{ localCounter }}</div>
+  <button @click="clickHandler">ボタンですよ</button>
+</template>
---
+++ src/views/ButtonView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CounterButton from '@/components/CounterButton.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <CounterButton />
+    </div>
+    <div>
+      <CounterButton />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
