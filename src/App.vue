f5819bd8fe2d1533c94ad308579e837acba189e9
Eiji Hachiya <hachiyae@gmail.com>
2024-03-09 11:11:43 +0900

    fix: cssの名前を分けた
    子コンポーネントのルートノードは親のCSSの影響を受けるらしい
    いらねぇ...
    https://ja.vuejs.org/api/sfc-css-features.html#child-component-root-elements

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import ReloadNotification from '@/components/common/ReloadNotification.vue'
+import Menu from '@/components/common/Menu.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <Menu />
+    <ReloadNotification />
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin-top: 16px;
+  margin-left: 16px;
+}
+</style>
---
+++ src/components/common/Menu.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="menus">
+    <router-link to="/about">About</router-link>
+    <router-link to="/button">Button</router-link>
+    <router-link to="/pokemon">Pokemon</router-link>
+  </div>
+</template>
+
+<style scoped>
+.menus {
+  display: flex;
+  gap: 8px;
+}
+</style>
